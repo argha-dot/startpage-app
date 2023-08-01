66b3c2f7c2f36471b00a25c61e194c4fd7bc8778
Argha Chakrabarty <ac380012@gmail.com>
2023-08-02 00:23:57 +0530

    revert the changes on main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -3,17 +3,17 @@ import ReactDOM from 'react-dom/client'
 import App from '@/pages/app.tsx'
 import '@/styles/index.css'
 import 'react-loading-skeleton/dist/skeleton.css'
-import { registerSW } from "virtual:pwa-register"
-
-
-// add this to prompt for a refresh
-const updateSW = registerSW({
-  onNeedRefresh() {
-    if (confirm("New content available. Reload?")) {
-      updateSW(true);
-    }
-  },
-});
+// import { registerSW } from "virtual:pwa-register"
+//
+//
+// // add this to prompt for a refresh
+// const updateSW = registerSW({
+//   onNeedRefresh() {
+//     if (confirm("New content available. Reload?")) {
+//       updateSW(true);
+//     }
+//   },
+// });
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
