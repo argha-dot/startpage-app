c16614299052687a4c03555a03e3c6012d1b5f8b
Argha Chakrabarty <ac380012@gmail.com>
2023-08-02 00:46:55 +0530

    pwa reload thingy

--- src/main.tsx
+++ src/main.tsx
@@ -3,20 +3,12 @@ import ReactDOM from 'react-dom/client'
 import App from '@/pages/app.tsx'
 import '@/styles/index.css'
 import 'react-loading-skeleton/dist/skeleton.css'
-// import { registerSW } from "virtual:pwa-register"
-//
-//
-// // add this to prompt for a refresh
-// const updateSW = registerSW({
-//   onNeedRefresh() {
-//     if (confirm("New content available. Reload?")) {
-//       updateSW(true);
-//     }
-//   },
-// });
+import ReloadPrompt from '@/components/reloadPrompt'
+
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
+    <ReloadPrompt />
     <App />
   </React.StrictMode>,
 )
